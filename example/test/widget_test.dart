8f93b5bba62e23b1eb47b95124b2b1642896b505
Chenzedeng <chenzedeng@geexfinance.com>
2021-09-15 16:08:39 +0800

    Init Project

---
+++ example/test/widget_test.dart
@@ -0,0 +1,24 @@
+/*
+ * @Description: 
+ * @Author: chenzedeng
+ * @Date: 2021-07-24 16:05:18
+ * @LastEditTime: 2021-09-15 16:07:03
+ */
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_app_upgrade_example/main.dart';
+
+void main() {
+  testWidgets('Verify Platform version', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+  });
+}
